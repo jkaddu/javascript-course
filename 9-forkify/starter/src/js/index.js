e99d2c0bee71e6cbb08a8d697531a1d050faacf6
jkaddu <jkaddu@yahoo.com>
2020-03-25 15:15:14 -0700

    first async func using axios

--- 9-forkify/starter/src/js/index.js
+++ 9-forkify/starter/src/js/index.js
@@ -1,11 +1,10 @@
 // Global app controller
-import string from "./models/Search";
 // import { add, multiply, ID } from "./views/SearchView";
 // how import everything from a another file/folder
-import * as searchView from "./views/SearchView";
-console.log(
-  `Using imported functions! ${searchView.add(
-    searchView.ID,
-    3
-  )} and ${searchView.multiply(5, 3)}`
-);
+// import * as searchView from "./views/SearchView";
+// console.log(
+//   `Using imported functions! ${searchView.add(
+//     searchView.ID,
+//     3
+//   )} and ${searchView.multiply(5, 3)}`
+// );

4422321fe4975a2815beb6217945a9763903b29e
jkaddu <jkaddu@yahoo.com>
2020-03-27 11:40:50 -0700

    added loader in UI

--- 9-forkify/starter/src/js/index.js
+++ 9-forkify/starter/src/js/index.js
@@ -1,10 +1,39 @@
-// Global app controller
-// import { add, multiply, ID } from "./views/SearchView";
-// how import everything from a another file/folder
-// import * as searchView from "./views/SearchView";
-// console.log(
-//   `Using imported functions! ${searchView.add(
-//     searchView.ID,
-//     3
-//   )} and ${searchView.multiply(5, 3)}`
-// );
+import Search from "./models/Search";
+import * as searchView from "./views/SearchView";
+import { elements, renderLoader, clearLoader } from "./views/base";
+
+/** Global state of the app
+ *  search object
+ * current recipe
+ * shopping list object
+ * liked recipes
+ */
+
+const state = {};
+
+const controlSearch = async () => {
+  // 1) get query from view
+  const query = searchView.getInput();
+
+  if (query) {
+    // 2) new search object and add to state
+    state.search = new Search(query);
+
+    // 3) prepare UI for results
+    searchView.clearInput();
+    searchView.clearResults();
+    renderLoader(elements.searchRes);
+
+    // 4) search for recipes
+    await state.search.getResults();
+
+    // 5) render results on UI
+    clearLoader();
+    searchView.renderResults(state.search.result);
+  }
+};
+
+elements.searchForm.addEventListener("submit", e => {
+  e.preventDefault();
+  controlSearch();
+});
--- 9-forkify/starter/src/js/views/SearchView.js
+++ 9-forkify/starter/src/js/views/SearchView.js
@@ -1,3 +1,53 @@
-// export const add = (a, b) => a + b;
-// export const multiply = (a, b) => a * b;
-// export const ID = 99;
+import { elements } from "./base";
+
+export const getInput = () => elements.searchInput.value;
+
+export const clearInput = () => {
+  elements.searchInput.value = "";
+};
+
+export const clearResults = () => {
+  elements.searchResList.innerHTML = "";
+};
+/*
+// 'Pasta with tomato and spinach'
+the acc starts at 0 then, acc + cur.length = 5 which is less than 17 then it pushes 
+the word to the designated array, newTitle = ['Pasta'] and repeats the process with 
+each iteration until the title is in there that is under the limit of 17
+*/
+const limitRecipeTitle = (title, limit = 17) => {
+  const newTitle = [];
+  if (title.length > limit) {
+    title.split(" ").reduce((acc, cur) => {
+      if (acc + cur.length <= limit) {
+        newTitle.push(cur);
+      }
+      return acc + cur.length;
+    }, 0);
+
+    // return the result
+    return `${newTitle.join(" ")} ...`;
+  }
+  return title;
+};
+
+const renderRecipe = recipe => {
+  const markup = `
+    <li>
+        <a class="results__link" href="#${recipe.recipe_id}">
+            <figure class="results__fig">
+                <img src="${recipe.image_url}" alt="${recipe.title}">
+            </figure>
+            <div class="results__data">
+                <h4 class="results__name">${limitRecipeTitle(recipe.title)}</h4>
+                <p class="results__author">${recipe.publisher_url}</p>
+            </div>
+        </a>
+    </li>
+    `;
+  elements.searchResList.insertAdjacentHTML("beforeend", markup);
+};
+
+export const renderResults = recipes => {
+  recipes.forEach(renderRecipe);
+};

f3b310e8c5008b0de0ee6f0be343455940fdd6d0
Joshua Kaddu <joshuakaddu@Joshuas-MacBook-Air.local>
2024-01-06 08:39:39 -0800

    implemented modal open/close for newRecipe

--- 16-Forkify/src/js/controller.js
+++ 16-Forkify/src/js/controller.js
@@ -4,6 +4,7 @@ import searchView from './views/searchView.js';
 import resultsView from './views/resultsView.js';
 import paginationView from './views/paginationView.js';
 import bookmarksView from './views/bookmarksView.js';
+import addRecipeView from './views/addRecipeView.js';
 
 import 'core-js/stable'; // Polyfiller
 import 'regenerator-runtime/runtime'; // Polyfiller for async functions
@@ -88,6 +89,12 @@ const controlBookmarks = function () {
   bookmarksView.render(model.state.bookmnarks);
 };
 
+const controlAddRecipe = function (newRecipe) {
+  console.log(newRecipe);
+
+  // Uploads new recipe data
+};
+
 const init = function () {
   bookmarksView.addHandlerRender(controlBookmarks);
   recipeView.addHandlerRender(controlRecipes);
@@ -95,6 +102,7 @@ const init = function () {
   recipeView.addHandlerAddBookmark(controlAddBookmark);
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
+  addRecipeView.addHandlerUpload(controlAddRecipe);
 };
 
 init();
